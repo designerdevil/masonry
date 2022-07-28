f891206d8bf493b6b13695f1129ea53a2064b3c1
Gaurav Mall <dev.gauravmall@gmail.com>
2022-07-28 20:10:44 +0530

    Grid with style variations

--- src/Masonry.js
+++ src/Masonry.js
@@ -4,50 +4,127 @@ function Masonry() {
     return (
         <>
             <h1>Masonry With Css Grid</h1>
-            <div className='masonry col4 gap2 rounded'>
+            <div className='masonry col4 gap3 rounded light imagebg'>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image1.webp' />
-                    <div>Item: <strong>1</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image1.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>1</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image2.webp' />
-                    <div>Item: <strong>2</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image2.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>2</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image3.webp' />
-                    <div>Item: <strong>3</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image3.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>3</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image4.webp' />
-                    <div>Item: <strong>4</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image4.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>4</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image5.webp' />
-                    <div>Item: <strong>5</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image5.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>5</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image6.webp' />
-                    <div>Item: <strong>6</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image6.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>6</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image7.webp' />
-                    <div>Item: <strong>7</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image7.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>7</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image8.webp' />
-                    <div>Item: <strong>8</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image8.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>8</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image9.webp' />
-                    <div>Item: <strong>9</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image9.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>9</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image10.webp' />
-                    <div>Item: <strong>10</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image10.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>10</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <img alt='myImage' src='./images/image11.webp' />
-                    <div>Item: <strong>11</strong></div>
+                    <div className='image-holder'><img alt='myImage' src='./images/image11.webp' /></div>
+                    <div className='item-indicator'>Item: <strong>11</strong></div>
+                    <div className='content-holder'>
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum dui massa, congue dignissim tellus egestas at. Ut in nisl viverra</p>
+                    </div>
+                    <div className='action-holder'>
+                        <a href="https://www.google.com" target="_blank" rel="noreferrer">Read more</a>
+                    </div>
                 </div>
             </div>
         </>
